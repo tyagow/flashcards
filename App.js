6561602ce115d1a4aef167e4c8fdab73a9a30008
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-12 20:33:14 -0300

    chore: add react-navigation and updating packages Resolves: #13

--- App.js
+++ App.js
@@ -1,23 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Platform, View, StatusBar } from "react-native"
+import { createBottomTabNavigator } from "react-navigation"
+import { Constants } from "expo"
+import { FontAwesome, Ionicons } from "@expo/vector-icons"
+
+import { purple, white } from "./utils/colors"
+import Home from "./screens/Home/"
+import NewDeck from "./screens/NewDeck/"
+
+function MyStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = createBottomTabNavigator(
+  {
+    home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={30} color={tintColor} />,
+      },
+    },
+    newDeck: {
+      screen: NewDeck,
+      navigationOptions: {
+        tabBarLabel: "New Deck",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-add-circle" size={30} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    navigationOptions: {
+      header: null,
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    },
+  },
+)
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MyStatusBar backgroundColor={purple} barStyle="light-content" />
+        <Tabs />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
