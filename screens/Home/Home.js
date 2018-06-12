f3e2d03ed09445adcc39c7d7543eca916195a54a
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-12 20:34:41 -0300

    feat: new home screen

---
+++ screens/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react"
+import { Text, View } from "react-native"
+
+export class Home extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text> Home </Text>
+      </View>
+    )
+  }
+}
+
+export default Home
---
+++ screens/NewDeck/NewDeck.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import { Text, View } from 'react-native'
+
+class NewDeck extends PureComponent {
+  render() {
+    return (
+      <View>
+        <Text> New Deck </Text>
+      </View>
+    )
+  }
+}
+
+export default NewDeck
