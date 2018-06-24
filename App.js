477ff4a9eba2ec7c2c03ae94ac91720c2dc37ec6
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-24 10:31:50 -0300

    feat: App with store configured

--- App.js
+++ App.js
@@ -1,70 +1,24 @@
-import React from "react"
-import { StyleSheet, Platform, View, StatusBar } from "react-native"
-import { createBottomTabNavigator } from "react-navigation"
-import { Constants } from "expo"
-import { FontAwesome, Ionicons } from "@expo/vector-icons"
+import React from 'react'
+import { StyleSheet, View, StatusBar } from 'react-native'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
+import { Constants } from 'expo'
 
-import { purple, white } from "./utils/colors"
-import Home from "./screens/Home/"
-import NewDeck from "./screens/NewDeck/"
+import store from './store/store'
+import { purple } from './utils/colors'
 
-function MyStatusBar({ backgroundColor, ...props }) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  )
-}
+import Tabs from './routes/AppTabsNavigation'
 
-const Tabs = createBottomTabNavigator(
-  {
-    home: {
-      screen: Home,
-      navigationOptions: {
-        tabBarLabel: "Home",
-        tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={30} color={tintColor} />,
-      },
-    },
-    newDeck: {
-      screen: NewDeck,
-      navigationOptions: {
-        tabBarLabel: "New Deck",
-        tabBarIcon: ({ tintColor }) => (
-          <Ionicons name="ios-add-circle" size={30} color={tintColor} />
-        ),
-      },
-    },
-  },
-  {
-    navigationOptions: {
-      header: null,
-    },
-    tabBarOptions: {
-      activeTintColor: Platform.OS === "ios" ? purple : white,
-      style: {
-        height: 56,
-        backgroundColor: Platform.OS === "ios" ? white : purple,
-        shadowColor: "rgba(0, 0, 0, 0.24)",
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowRadius: 6,
-        shadowOpacity: 1,
-      },
-    },
-  },
+const MyStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
 )
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MyStatusBar backgroundColor={purple} barStyle="light-content" />
-        <Tabs />
-      </View>
-    )
-  }
+MyStatusBar.propTypes = {
+  backgroundColor: PropTypes.string,
+}
+MyStatusBar.defaultProps = {
+  backgroundColor: purple,
 }
 
 const styles = StyleSheet.create({
@@ -72,3 +26,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 })
+
+export const App = () => (
+  <Provider store={store}>
+    <View style={styles.container}>
+      <MyStatusBar backgroundColor={purple} barStyle="light-content" />
+      <Tabs />
+    </View>
+  </Provider>
+)
+
+export default App
+
