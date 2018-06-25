ddbd60d95e95e33fff5a1b332e1c5bd19fbd4f50
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-25 06:33:01 -0300

    tests: refactoring tests

--- screens/NewDeck/NewDeck.test.js
+++ screens/NewDeck/NewDeck.test.js
@@ -5,11 +5,11 @@ import { NewDeck } from './NewDeck'
 
 describe('NewDeck Screen', () => {
   it('should render a label', () => {
-    const wrapper = shallow(<NewDeck createDeck={() => {}} />)
+    const wrapper = shallow(<NewDeck navigation={{}} createDeck={() => {}} />)
     expect(wrapper).toMatchSnapshot()
   })
   it('should have an input component', () => {
-    const wrapper = shallow(<NewDeck createDeck={() => {}} />)
+    const wrapper = shallow(<NewDeck navigation={{}} createDeck={jest.fn()} />)
     expect(wrapper.find('Styled(TextInput)').length).toBe(1)
   })
 })
