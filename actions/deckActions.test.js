e0e13694a69fdb5bf857870de75a379914bc3d04
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-25 06:16:54 -0300

    feat: new action DECK_SELECTED

--- actions/deckActions.test.js
+++ actions/deckActions.test.js
@@ -1,8 +1,16 @@
-import { NEW_DECK, createDeck } from "./deckActions"
+import { NEW_DECK, createDeck } from './deckActions'
 
-describe("CREATE_NEW_DECK", () => {
-  it("action should return expected object", () => {
-    const expected = { type: NEW_DECK, payload: "Title Deck" }
-    expect(createDeck("Title Deck")).toEqual(expected)
+describe('CREATE_NEW_DECK', () => {
+  it('action should return expected object', () => {
+    const expected = { type: NEW_DECK, payload: 'Title Deck' }
+    expect(createDeck('Title Deck')).toEqual(expected)
   })
 })
+
+// describe('DECK_SELECTED', () => {
+//   it("action should return expected object", () => {
+//     const expected = { type: DECK_SELECTED, payload: "Title Deck" }
+//     expect(createDeck("Title Deck")).toEqual(expected)
+//   })
+// })
+
