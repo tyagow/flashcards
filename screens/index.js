13f41c39aa2ddb93997b162bca6313892dc2ab76
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-25 06:34:58 -0300

    feat: screens are exported on index.js

---
+++ screens/index.js
@@ -0,0 +1,8 @@
+import HomeScreen from './Home'
+import NewDeckScreen from './NewDeck'
+import DeckDetailScreen from './DeckDetail'
+
+export const Home = HomeScreen
+export const NewDeck = NewDeckScreen
+export const DeckDetail = DeckDetailScreen
+
