087b1b31ce4c7cc38c410042a3c0ed9474fbb67f
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-26 06:42:10 -0300

    feat: new DeckDetail container

--- screens/DeckDetail/DeckDetail.js
+++ screens/DeckDetail/DeckDetail.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import styled from 'styled-components/native'
 
+const StyledButtonText = styled.Text`
+  background-color: #000;
+  text-align: center;
+  font-size: 22;
+  color: #fff;
+  margin: 30px;
+  height: 50;
+  padding-top: 10;
+  padding-bottom: 15;
+  margin-vertical: 10;
+`
 
 export class DeckDetail extends Component {
   render() {
-    const { title } = this.props.deck
+    const { title, id } = this.props.deck
     return (
       <View>
-        <Text>{title}</Text>
+        <Text>{id} - {title}</Text>
+        <TouchableOpacity onPress={this.newDeck}>
+          <StyledButtonText>Add Question</StyledButtonText>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 DeckDetail.propTypes = {
   deck: PropTypes.shape({
+    id: PropTypes.string,
     title: PropTypes.string,
   }).isRequired,
 }
+const mapStateToProps = state => ({ deck: state.decks.item })
 
-export default DeckDetail
+export default connect(mapStateToProps)(DeckDetail)
+
--- screens/DeckDetail/DeckDetail.test.js
+++ screens/DeckDetail/DeckDetail.test.js
@@ -4,13 +4,13 @@ import renderer from 'react-test-renderer'
 import { DeckDetail } from './DeckDetail'
 
 describe('DeckDetail', () => {
-  const deck = { title: 'Deck One' }
+  const deck = { title: 'Deck One', id: '1' }
   it('should match snapshot', () => {
     const wrapper = shallow(<DeckDetail deck={deck} />)
     expect(wrapper).toMatchSnapshot()
   })
   it('should render deck Title', () => {
     const wrapper = renderer.create(<DeckDetail deck={deck} />).toJSON()
-    expect(wrapper.children[0].children[0]).toEqual('Deck One')
+    expect(wrapper.children[0].children[2]).toEqual('Deck One')
   })
 })
