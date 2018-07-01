e58c34fa17635e35655d0b1598fa0267d5ecab10
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-01 14:56:33 -0300

    feat: add new middleware questionIjector

--- store/store.js
+++ store/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/index'
 import idInjector from '../middlewares/idInjector/idInjector'
+import questionInjector from '../middlewares/questionInjector/questionInjector'
 
 const initialState = {}
-const middleware = [idInjector]
+const middleware = [idInjector, questionInjector]
 
 const store = createStore(
   rootReducer,
