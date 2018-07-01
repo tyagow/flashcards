4c11e380a0c6af995d5cac4ed85e71c8b208e3a7
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-01 14:51:13 -0300

    feat: new route for creating new question

--- routes/HomeStackNavigation.js
+++ routes/HomeStackNavigation.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from 'react-navigation'
-import HomeScreen from './../screens/Home/Home'
-import DeckDetail from './../screens/DeckDetail/'
+import HomeScreen from '../screens/Home/Home'
+import DeckDetail from '../screens/DeckDetail'
+import NewQuestion from '../screens/NewQuestion'
+// import Quiz from '../screens/Quiz'
 
 const HomeStackNavigation = createStackNavigator({
   Home: {
@@ -10,5 +12,11 @@ const HomeStackNavigation = createStackNavigator({
   DeckDetail: {
     screen: DeckDetail,
   },
+  NewQuestion: {
+    screen: NewQuestion,
+  },
+  // Quiz: {
+  //   screen: Quiz,
+  // },
 })
 export default HomeStackNavigation
