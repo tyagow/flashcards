a82a107134cce5e0d3ef7768e9bb0162a3afd914
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-01 14:47:37 -0300

    refactor: changing tests folder

---
+++ actions/__tests__/deckActions.test.js
@@ -0,0 +1,15 @@
+import { NEW_DECK, createDeck, DECK_SELECTED, selectDeck } from '../deckActions'
+
+describe('CREATE_NEW_DECK', () => {
+  it('action should return expected object', () => {
+    const expected = { type: NEW_DECK, payload: 'Title Deck' }
+    expect(createDeck('Title Deck')).toEqual(expected)
+  })
+})
+
+describe('DECK_SELECTED', () => {
+  it('action should return expected object', () => {
+    const expected = { type: DECK_SELECTED, payload: 'Title Deck' }
+    expect(selectDeck('Title Deck')).toEqual(expected)
+  })
+})
