9923f94e25076b3714dda4b89b1cb6faaa9378d0
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-04 19:22:00 -0300

    refactor: removing console.log

--- selectors/questionGameSelector.js
+++ selectors/questionGameSelector.js
@@ -6,10 +6,7 @@ const getQuestionsAnswered = (state, props) => state.answered
 const getQuizQuestion = createSelector(
   [getQuestions, getQuestionsAnswered],
   (questions, answers) => {
-    console.log(answers)
     noAnsweredQuestions = questions.filter((e) => {
-      console.log(e.id)
-      console.log(answers.indexOf(e.id))
       return answers.indexOf(e.id) < 0
     })
     return noAnsweredQuestions[0] || {}
