fc4f9e1fe06e0d9736d17a30380e059d15997559
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-06 15:25:32 -0300

    style: styling header

---
+++ styles/header.js
@@ -0,0 +1,15 @@
+import { blue } from '../utils/colors'
+
+export const headerBackgroundColor = blue
+
+export const headerStyle = {
+  headerTintColor: '#fff',
+  headerStyle: {
+    height: 30,
+    backgroundColor: headerBackgroundColor,
+  },
+  headerTitleStyle: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+}
