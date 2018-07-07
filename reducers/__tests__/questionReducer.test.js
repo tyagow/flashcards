e29f82da876685a92e93e7bd2f3da9f6a9376395
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-07 16:08:05 -0300

    feat: action to handle update questions

--- reducers/__tests__/questionReducer.test.js
+++ reducers/__tests__/questionReducer.test.js
@@ -1,5 +1,5 @@
 import questionReducer from '../questionReducer'
-import { NEW_QUESTION } from '../../actions/questionActions'
+import { NEW_QUESTION, UPDATE_QUESTIONS } from '../../actions/questionActions'
 
 describe('questionReducer', () => {
   it('should handle initial state', () => {
@@ -10,7 +10,19 @@ describe('questionReducer', () => {
   })
   it('should add new deck when action = NEW_QUESTION', () => {
     const question = { title: 'New Question' }
-    expect(questionReducer(undefined, { type: NEW_QUESTION, payload: question }))
-      .toEqual({ items: [question], item: {} })
+    expect(questionReducer(undefined, { type: NEW_QUESTION, payload: question })).toEqual({
+      items: [question],
+      item: {},
+    })
+  })
+  it('should update items with given payload', () => {
+    const obj = [{ id: 2 }]
+    expect(questionReducer(
+      {
+        items: [],
+        item: {},
+      },
+      { type: UPDATE_QUESTIONS, payload: obj },
+    )).toEqual({ items: obj, item: {} })
   })
 })
