4bb4244df6c7050affc15f3b1d5f0f1361c291ca
Tiago Almeida <tyagow@hotmail.com.br>
2018-07-07 09:14:02 -0300

    tests: fix tests

--- selectors/__tests__/questionSelector.test.js
+++ selectors/__tests__/questionSelector.test.js
@@ -2,12 +2,13 @@ import getQuizQuestions from '../questionSelector'
 
 describe('questionSelector', () => {
   const state = {
+    questions: { items: [{ id: 1 }, { id: 2 }, { id: 3 }] },
     decks: {
       item: { questions: [1, 2] },
-      items: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      items: [],
     },
   }
-  it('should select all objects questions from state.decks.items filtering only those who`s ids are inside state.decks.item.questions ', () => {
+  it('should select all objects questions from state.questions.items filtering only those who`s ids are inside state.decks.item.questions ', () => {
     expect(getQuizQuestions(state)).toEqual([{ id: 1 }, { id: 2 }])
   })
 })
